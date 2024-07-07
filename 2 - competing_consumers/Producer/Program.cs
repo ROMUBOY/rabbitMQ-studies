05f79bac1a8fa4c0721eb32047f49e7e398c86c5
Romualdo "Romuboy" Santos <romualdoosfilho94@gmail.com>
2024-07-07 12:23:34 -0300

    First commit: first app and competing consumers

---
+++ 2 - competing_consumers/Producer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory{ HostName = "localhost"};
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "letterbox",
+    durable:false,
+    exclusive:false,
+    autoDelete:false,
+    arguments:null
+);
+
+var random = new Random();
+
+var messageId = 1;
+
+while(true)
+{
+    var publishingTime = random.Next(1, 4);
+
+    var message = $"sending MessageId: {messageId}";
+
+    var encodedMessage = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish("", "letterbox", null, encodedMessage);
+
+    Console.WriteLine($"Published message: {message}");
+
+    Task.Delay(TimeSpan.FromSeconds(publishingTime)).Wait();
+
+    messageId++;
+}
+
