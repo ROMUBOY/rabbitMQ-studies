1f4604305241988fcc438b72e3af950075acdcfe
Romualdo "Romuboy" Santos <romualdoosfilho94@gmail.com>
2024-07-16 08:17:29 -0300

    routing topic exchange

--- 3 - pub_sub/FirstConsumer/obj/Debug/net8.0/FirstConsumer.AssemblyInfo.cs
+++ 3 - pub_sub/FirstConsumer/obj/Debug/net8.0/FirstConsumer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstConsumer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25ac105162e269cf5ec0c02a6d4f016a8d41718f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d044149fd14ceaf3f5eff5d8d1c4b5cd28b6bae")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstConsumer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstConsumer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ 4.2 - routing topic exchange/Producer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory{ HostName = "localhost"};
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "mytopicexchange", ExchangeType.Topic);
+
+var userPaymentMessage = "A european user paid something";
+
+var userPaymentenCodedMessage = Encoding.UTF8.GetBytes(userPaymentMessage);
+
+channel.BasicPublish("mytopicexchange", "user.europe.payments", null, userPaymentenCodedMessage);
+
+Console.WriteLine($"Send message: {userPaymentMessage}");
+
+var businessOrderMessage = "A european business ordered something";
+
+var businessOrderCodedMessage = Encoding.UTF8.GetBytes(businessOrderMessage);
+
+channel.BasicPublish("mytopicexchange", "business.europe.order", null, businessOrderCodedMessage);
+
+Console.WriteLine($"Send message: {businessOrderMessage}");
